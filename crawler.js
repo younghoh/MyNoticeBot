42738b94ee7fc4ca2f154e36c7d1c8b4c48d2a94
youngho jeong <hooo5513@naver.com>
2017-10-21 02:33:29 +0900

    add crawling Function

--- crawler.js
+++ crawler.js
@@ -7,8 +7,54 @@ var CLIENT_EVENTS = require('@slack/client').CLIENT_EVENTS;
 var token = process.env.SLACK_API_TOKEN || '';
 
 // 추가
-function crawlingFunc(baseurl){
+function equalArray(array, tempArray){
+    let resultArray = [];
+    for(i = 0; i < array.length ; i++){
+        if(array.indexOf(tempArray[i]) === -1){
+            resultArray.push(i);
+        }
+    }
+    return resultArray;
 }
+
+function crawlingFunc(baseurl, timer){
+    var titleArray = [];
+    var flag = true;
+    var options = {
+        url : baseurl,
+        headers : {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.110 Safari/537.36',
+            'Content-Type' : 'application/x-www-form-urlencoded' 
+        }
+    }
+    return () => setInterval(() => request(options, function(error, response, body){
+        if (error) throw error;
+        var $ = cheerio.load(body);
+        var tempTitleArray = [];
+        var tempArray = [];
+        $(".bd_lst.bd_tb_lst.bd_tb > tbody tr").each(function(){
+            var no = $(this).find('.no').text().trim();
+            var title = $(this).find('.title > a').text().trim();
+            var href = $(this).find('.title > a').attr('href');
+            if(flag) titleArray.push(title);
+            tempTitleArray.push(title);
+            tempArray.push({
+                title : title,
+                no : no,
+                href : href
+            });
+        });
+        flag = false;
+        var resultArray = equalArray(titleArray, tempTitleArray);
+        if(resultArray.length !== 0){
+            for(i = 0; i < resultArray.length ; i++){
+                rtm.sendMessage(`${tempArray[resultArray[i]].no} ${tempArray[resultArray[i]].title} ${tempArray[resultArray[i]].href}`, channel);                            
+            }
+            flag = true;
+        }
+    }),timer * 1000);
+}
+
 
 var rtm = new RtmClient(token, {logLevel: 'debug'});
 rtm.start();
@@ -22,27 +68,15 @@ rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, (rtmStartData) => {
 
 rtm.on(CLIENT_EVENTS.RTM.RTM_CONNECTION_OPENED, function(){
     rtm.sendMessage("정상적으로 연결", channel);
-    var options = {
-        url : "http://computer.cnu.ac.kr/index.php?mid=notice",
-        headers : {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.110 Safari/537.36',
-            'Content-Type' : 'application/x-www-form-urlencoded' 
-        }
-    }
-    setInterval(() => request(options, function(error, response, body){
-        if (error) throw error;
-        var $ = cheerio.load(body);
-        //console.log($(".title > a"));
-        // $(".title > a").each(function(){
-        $(".bd_lst.bd_tb_lst.bd_tb > tbody tr").each(function(){
-            var no = $(this).find('.no').text().trim();
-            var title = $(this).find('.title > a').text().trim();
-            var href = $(this).find('.title > a').attr('href');
-            rtm.sendMessage(`${no} ${title} ${href}`, channel);
-            //console.log(title);
-        });
-        //console.log(body);
-    }),10000);
+
+    let crawlingNotice = crawlingFunc("http://computer.cnu.ac.kr/index.php?mid=notice", 10);
+    let crawlingGeneralNotice = crawlingFunc("http://computer.cnu.ac.kr/index.php?mid=gnotice", 10);
+    let crawlingSWUnivNotice = crawlingFunc("http://computer.cnu.ac.kr/index.php?mid=saccord", 10);
+    let crawlingJobNotice = crawlingFunc("http://computer.cnu.ac.kr/index.php?mid=job", 10);
+    crawlingNotice();
+    crawlingGeneralNotice();
+    crawlingSWUnivNotice();
+    crawlingJobNotice();
 })
 
 
@@ -55,6 +89,5 @@ rtm.on(RTM_EVENTS.MESSAGE, function (message){
         rtm.sendMessage("안녕", channel);
     }
 })
-// var baseurl = "http://computer.cnu.ac.kr/index.php?mid=notice";
 
 
